d05204aa635634961faa0cc5676083c5ff662ac1
hachetk <hachetk_5@outlook.com>
2022-07-11 13:52:44 -0400

    Se agrega archivo de prueba

--- index.php
+++ index.php
@@ -1,90 +1,179 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Página web </title>
-    <link rel="shortcut icon" href="" type="images/x-icon">
-    <link rel="stylesheet" href="css/estilos.css">
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
-</head>
-
-<body>
-    <header>
-        <nav>
-
-
-            <a href="login.php">Iniciar sesión</a>
-        </nav>
-        <section class="textos-header">
-            <h1>Comunidad agricola</h1>
-            <h2>Invernaderos Hidropónicos</h2>
-        </section>
-
-        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
-                style="height: 100%; width: 100%;">
-                <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
-                    style="stroke: none; fill: rgb(255, 255, 255);"></path>
-            </svg></div>
-    </header>
-
-    <main>
-        <section class="contenedor sobre-nosotros">
-            <h2 class="titulo">Nuestro productos</h2>
-            <div class="contenedor-sobre-nosotros">
-                <div class="contenido-textos">
-                    <h3 aling="center"><span>1</span>Frutas y Verduras</h3>
-                    <p> hoy nuestras frutas viajan por el Territorio Nacional para satisfacer clientes de todas las latitudes, nuestros productos tienen como destino los mercados de mayor exigencia y estandares de calidad.</p>
-                    <h3><span>2</span>Calidad y Tiempo</h3>
-                    <p>Calidad a Tiempo semillas certificadas y las mejores variedades para asegurar la calidad y produccion de nuestros productos, lo que nos permite brindarle a nuestros clientes una mayor flexibilidad y agilidad en nuestros servicios.</p>
-                </div>
-            </div>
-        </section>
-
-        <section class="about-services">
-            <div class="contenedor">
-                <h2 class="titulo">Nuestros servicios</h2>
-                <div class="servicio-cont">
-                    <div class="servicio-ind">
-                        <h3>Filtracion Industrial</h3>
-                        <p>Representamos en Chile los mejores equipos de filtracion industrial para distintas aplicaciones como proteccion de sistemas de osmosis, recuperacion de agua en torres de refrigeracion,proteccion de equipos industriales entre otras</p>
-                    </div>
-                    <div class="servicio-ind">
-                        
-                        <h3>Control De Olores</h3>
-                        <p>Una amplia gama de sistemas para el control de olores en un formato simple, flexible a la medidas de sus procesos con PI2 technologies para la solucion a estos problemas.</p>
-                    </div>
-                    <div class="servicio-ind">
-                        <h3>Tratamiento Del Agua</h3>
-                        <p>Parte importante de los procesos industriales es el control de temperatura. En Chile contamos con la linea completa de REYMSA torres de refrigeracion fabricadas con el mas alto estandar y certificados en eficiencia.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
-
-    <footer>
-        <div class="contenedor-footer">
-            <div class="content-foo">
-                <h4>Telefono</h4>
-                <p>2830192830</p>
-            </div>
-
-            <div class="content-foo">
-                <h4>Email</h4>
-                <p>Mhglobal@Mhglobal.cl</p>
-            </div>
-
-            <div class="content-foo">
-                <h4>Dirección</h4>
-                <p>Los Cactus #321</p>
-            </div>
-
-        </div>
-    </footer>
-
-</body>
+<!DOCTYPE html>
+
+<html lang="es">
+
+
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Página web </title>
+
+    <link rel="shortcut icon" href="" type="images/x-icon">
+
+    <link rel="stylesheet" href="css/estilos.css">
+
+    
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
+
+</head>
+
+  
+
+<body>
+
+    <header>
+
+        <nav>
+
+
+
+
+
+            <a href="login.php">Iniciar sesión</a>
+
+        </nav>
+
+        <section class="textos-header">
+
+            <h1>Comunidad agricola</h1>
+
+            <h2>Invernaderos Hidropónicos</h2>
+
+        </section>
+
+
+
+        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+
+                style="height: 100%; width: 100%;">
+
+                <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+
+                    style="stroke: none; fill: rgb(255, 255, 255);"></path>
+
+            </svg></div>
+
+    </header>
+
+
+
+    <main>
+
+        <section class="contenedor sobre-nosotros">
+
+            <h2 class="titulo">Nuestro productos</h2>
+
+            <div class="contenedor-sobre-nosotros">
+
+                <div class="contenido-textos">
+
+                    <h3 aling="center"><span>1</span>Frutas y Verduras</h3>
+
+                    <p> hoy nuestras frutas viajan por el Territorio Nacional para satisfacer clientes de todas las latitudes, nuestros productos tienen como destino los mercados de mayor exigencia y estandares de calidad.</p>
+
+                    <h3><span>2</span>Calidad y Tiempo</h3>
+
+                    <p>Calidad a Tiempo semillas certificadas y las mejores variedades para asegurar la calidad y produccion de nuestros productos, lo que nos permite brindarle a nuestros clientes una mayor flexibilidad y agilidad en nuestros servicios.</p>
+
+                </div>
+
+            </div>
+
+        </section>
+
+
+
+        <section class="about-services">
+
+            <div class="contenedor">
+
+                <h2 class="titulo">Nuestros servicios</h2>
+
+                <div class="servicio-cont">
+
+                    <div class="servicio-ind">
+
+                        <h3>Filtracion Industrial</h3>
+
+                        <p>Representamos en Chile los mejores equipos de filtracion industrial para distintas aplicaciones como proteccion de sistemas de osmosis, recuperacion de agua en torres de refrigeracion,proteccion de equipos industriales entre otras</p>
+
+                    </div>
+
+                    <div class="servicio-ind">
+
+                        
+
+                        <h3>Control De Olores</h3>
+
+                        <p>Una amplia gama de sistemas para el control de olores en un formato simple, flexible a la medidas de sus procesos con PI2 technologies para la solucion a estos problemas.</p>
+
+                    </div>
+
+                    <div class="servicio-ind">
+
+                        <h3>Tratamiento Del Agua</h3>
+
+                        <p>Parte importante de los procesos industriales es el control de temperatura. En Chile contamos con la linea completa de REYMSA torres de refrigeracion fabricadas con el mas alto estandar y certificados en eficiencia.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+
+    <footer>
+
+        <div class="contenedor-footer">
+
+            <div class="content-foo">
+
+                <h4>Telefono</h4>
+
+                <p>2830192830</p>
+
+            </div>
+
+
+
+            <div class="content-foo">
+
+                <h4>Email</h4>
+
+                <p>Mhglobal@Mhglobal.cl</p>
+
+            </div>
+
+
+
+            <div class="content-foo">
+
+                <h4>Dirección</h4>
+
+                <p>Los Cactus #321</p>
+
+            </div>
+
+
+
+        </div>
+
+    </footer>
+
+
+
+</body>
+
 </html>
